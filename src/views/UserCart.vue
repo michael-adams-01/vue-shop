38390ad1002678414047bc70cf9b286f42e8fe5b
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-03-31 17:43:54 -0500

    29-03-2025-1743288949

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -1,27 +1,6 @@
 <template>
-  <base-card>
-    <h1>{{ items }}</h1>
-    <h1 class="text-center font-bold text-2xl">Your Cart</h1>
-    <base-card>
-      <ul v-for="item in items">
-        <li>{{ item.name }}</li>
-      </ul>
-    </base-card>
-  </base-card>
+  <h1>Shopping Cart</h1>
 </template>
 
 <script>
-import { useShopStore } from '../stores/shop.js';
-export default {
-  setup() {
-    const store = useShopStore();
-    return { store };
-  },
-  computed: {
-    items() {
-      console.log(this.store.userCart[0])
-      return this.store.products.find(item => item.id === this.store.userCart[1]);
-    }
-  }
-}
 </script>
