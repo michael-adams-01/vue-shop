6f2b7f852d200bc47430cc80ce4b3eacf250987a
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-03-31 18:55:51 -0500

    Add placeholder image

--- src/views/ProductsList.vue
+++ src/views/ProductsList.vue
@@ -4,6 +4,8 @@
       <h1 class="text-center font-bold text-2xl">Featured Products</h1>
       <ul v-for="product in store.products">
         <base-card @click="loadProduct(product.id)">
+          <img class="h-130 w-200 bg-blue-500 bg-cover bg-no-repeat bg-center"
+            src="https://www.aaronfaber.com/wp-content/uploads/2017/03/product-placeholder-wp.jpg" alt="">
           <li>Name: {{ product.name }}</li>
           <li class="font-bold">Image placeholder</li>
           <li>Description: {{ product.description }}</li>
