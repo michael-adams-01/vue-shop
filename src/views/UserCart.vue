7f0c673082555cd970dc7252de7df9f1ad212e67
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-01 11:52:36 -0500

    Add pinia to UserCart

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -1,6 +1,14 @@
 <template>
   <h1>Shopping Cart</h1>
+  <h3>{{ store.userCart }}</h3>
 </template>
 
 <script>
+import { useShopStore } from '../stores/shop.js';
+export default {
+  setup() {
+    const store = useShopStore();
+    return { store };
+  }
+}
 </script>
