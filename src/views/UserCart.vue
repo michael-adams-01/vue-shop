e076c6ee44b2762ccaf7440cb68acd75d9e42a02
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-02 10:33:35 -0500

    Attempt 1

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -1,6 +1,15 @@
 <template>
-  <h1>Shopping Cart</h1>
-  <h3>{{ store.userCart }}</h3>
+  <div>
+    <h1>Cart items {{ getCartItems }}</h1>
+    <h1 class="text-center text-2xl font-bold">Shopping Cart</h1>
+    <base-card>
+      <ul v-for="item in store.userCart">
+        <base-card>
+          <li>{{ item }}</li>
+        </base-card>
+      </ul>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,14 @@ export default {
   setup() {
     const store = useShopStore();
     return { store };
+  },
+  computed: {
+    getCartItems() {
+      //const testArray = ['1', '2']
+      //const cartItems = this.store.userCart.filter(item => this.store.userCart.includes(item.id));
+      const cartItems = this.store.products.filter(item => item.id === this.store.cartItems);
+      return cartItems
+    }
   }
 }
 </script>
