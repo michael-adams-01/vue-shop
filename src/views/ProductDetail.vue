1cc0e0424d692f5f74248d05e154b798cb11eac1
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-07 12:16:06 -0500

    Add checkout or continue shopping prompt after adding to cart

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <h1>{{ store.userCart }}</h1>
     <base-card>
       <h1 class="text-center font-bold text-2xl">{{ productData.name }}</h1>
       <p>{{ productData.description }}</p>
       <h3>${{ productData.price }}</h3>
       <h3>Items left in stock: {{ productData.stock }}</h3>
       <h3>Rating: {{ productData.rating }}</h3>
-      <base-button @click="addToCart(productData.id)" class="mt-2">Add to cart</base-button>
+      <base-button @click="addToCart(productData.id)" class="mt-2 hover:scale-95">Add to cart</base-button>
+    </base-card>
+    <base-card v-if="showGoToCart">
+      <h1 class="font-bold text-2xl">Ready to checkout?</h1>
+      <router-link
+        class="bg-blue-500 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        to="/">Continue Shopping</router-link>
+      <router-link
+        class="bg-blue-500 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        to="/cart">Checkout</router-link>
     </base-card>
   </div>
 </template>
@@ -19,6 +27,11 @@ export default {
     const store = useShopStore();
     return { store };
   },
+  data() {
+    return {
+      showGoToCart: false,
+    }
+  },
   computed: {
     productData() {
       return this.store.products.find(product => product.id === this.$route.params.id);
@@ -27,7 +40,8 @@ export default {
   methods: {
     addToCart(itm) {
       this.store.userCart.push(itm);
+      this.showGoToCart = true;
     }
-  }
+  },
 }
 </script>
