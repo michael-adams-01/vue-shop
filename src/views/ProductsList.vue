8aebefc9ad3bf8f53529d16b291027b8b2a71cb9
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-07 12:17:09 -0500

    Add More Details button to ProductsList

--- src/views/ProductsList.vue
+++ src/views/ProductsList.vue
@@ -12,6 +12,9 @@
           <li>Price: ${{ product.price }}</li>
           <li>Stock: {{ product.stock }}</li>
           <li>Rating: {{ product.rating }}</li>
+          <router-link
+            class="bg-blue-500 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">More
+            details</router-link>
         </base-card>
       </ul>
     </base-card>
