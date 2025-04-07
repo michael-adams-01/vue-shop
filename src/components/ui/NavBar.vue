f24fd0c0909a9ae5c43fc2a75c0a2ca81e654f42
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-07 11:41:02 -0500

    Add counter to shopping cart in navbar

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="border-b-1 border-gray-400 w-full p-3 flex justify-between">
     <router-link to="/" class="text-green-600 font-bold text-4xl">VueShop</router-link>
-    <router-link to="/cart" class="text-2xl">Cart</router-link>
+    <router-link to="/cart" class="text-2xl">Cart {{ store.cartItemsCount }}</router-link>
   </div>
 </template>
 
 
-<script></script>
+<script>
+import { useShopStore } from '@/stores/shop.js';
+export default {
+  setup() {
+    const store = useShopStore();
+    return { store };
+  }
+}
+</script>
