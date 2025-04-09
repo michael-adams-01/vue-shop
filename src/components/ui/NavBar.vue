4d11939519716a119e52fda48b77fae6d060de45
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-04-09 16:12:37 -0500

    Add cart svg

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="border-b-1 border-gray-400 w-full p-3 flex justify-between">
     <router-link to="/" class="text-green-600 font-bold text-4xl">VueShop</router-link>
-    <router-link to="/cart" class="text-2xl">Cart {{ store.cartItemsCount }}</router-link>
+    <router-link to="/cart" class="text-2xl">
+      <img class="w-10 inline" src="/cart.svg" alt="">
+      {{ store.cartItemsCount }}</router-link>
   </div>
 </template>
 
